2efcd3921e4f32d92ccde714147238a0ecccade2
Husain Bhagat <hussainbhagat52@gmail.com>
2025-04-13 16:38:36 +0530

    Add error handling middleware and configure Express to use cookie parser and JSON body parsing

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import subscriptionRoutes from './routes/subscription.routes.js';
 import { connectToDatabase } from './database/mongodb.js';
-
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/subscriptions', subscriptionRoutes);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
   res.send('Welcome to Subify API');
